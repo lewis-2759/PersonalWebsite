ca61cbbeb189f04fbe0e357e3e6ea051a5a48283
Andrew Lewis <lewis.2759@osu.edu>
2025-01-14 17:31:38 -0500

    changed styling, added resume

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav ul li a');
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
     console.log('Document is ready');
 });
