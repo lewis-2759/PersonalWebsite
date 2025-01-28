2f294b687872e07f3033a1a422dfdc95fe4ba6b2
Andrew Lewis <lewis.2759@osu.edu>
2025-01-28 15:14:21 -0500

    updated images, added JungleRPG to projects

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
             event.preventDefault();
             const targetId = link.getAttribute('href').substring(1);
             const targetSection = document.getElementById(targetId);
-            const headerOffset = document.querySelector('nav').offsetHeight
+            const headerOffset = 30;
             const elementPosition = targetSection.getBoundingClientRect().top;
-            const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+            const offsetPosition = elementPosition + window.scrollY - headerOffset;
 
             window.scrollTo({
                 top: offsetPosition,
@@ -18,12 +18,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const navbar = document.getElementById('navbar');
     const sticky = navbar.offsetTop;
+    const navbarPlaceholder = document.createElement('div');
+    navbarPlaceholder.style.height = `${navbar.offsetHeight}px`;
+    navbar.parentNode.insertBefore(navbarPlaceholder, navbar);
 
     window.addEventListener('scroll', () => {
-        if (window.pageYOffset >= sticky) {
+        if (window.scrollY >= sticky) {
             navbar.classList.add('sticky');
+            navbarPlaceholder.style.display = 'block';
         } else {
             navbar.classList.remove('sticky');
+            navbarPlaceholder.style.display = 'none';
         }
     });
 });
